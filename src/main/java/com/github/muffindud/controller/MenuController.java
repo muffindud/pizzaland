a716ebcfa99f9d75f918718fe99b3974890fe73e
muffindud <ccatlabuga2002@gmail.com>
2025-07-07 10:40:43 +0300

    add discount notification messages

--- src/main/java/com/github/muffindud/controller/MenuController.java
+++ src/main/java/com/github/muffindud/controller/MenuController.java
@@ -1,5 +1,6 @@
 package com.github.muffindud.controller;
 
+import com.github.muffindud.config.ConfigProvider;
 import com.github.muffindud.enums.NotificationTopic;
 import com.github.muffindud.listener.EventListener;
 
@@ -8,8 +9,27 @@ public final class MenuController implements EventListener {
 
     }
 
+    private void sendDiscountApplied() {
+        System.out.printf("You're about to spend %.2f. You won %.2f%%",
+                ConfigProvider.getDiscount().discountPrice(),
+                ConfigProvider.getDiscount().discountRate() * 100);
+    }
+
+    private void sendDiscountNotApplied() {
+        System.out.printf("You're no longer over %.2f. %.2f%% discount no longer applied",
+                ConfigProvider.getDiscount().discountPrice(),
+                ConfigProvider.getDiscount().discountPrice() * 100);
+    }
+
     @Override
     public void update(NotificationTopic topic) {
-
+        switch (topic) {
+            case DISCOUNT_APPLIED:
+                sendDiscountApplied();
+            case DISCOUNT_NOT_APPLIED:
+                sendDiscountNotApplied();
+            default:
+                throw new RuntimeException("Operation not supported: " + topic);
+        }
     }
 }
