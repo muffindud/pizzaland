3d86f78e8ef152ca7f85538c693f186db9571c93
muffindud <ccatlabuga2002@gmail.com>
2025-07-07 16:14:48 +0300

    add space in cartview

--- src/main/java/com/github/muffindud/view/CartView.java
+++ src/main/java/com/github/muffindud/view/CartView.java
@@ -16,7 +16,7 @@ public final class CartView {
                 IntStream.range(1, cart.getProductQty().size() + 1)
                         .mapToObj(i -> {
                             Map.Entry<Product, Integer> entry = cartEntryIterator.next();
-                            return "[" + i + "]." + entry.getValue() + " x " + entry.getKey().getName() + ": " + entry.getKey().getPrice() * entry.getValue() + "\n";
+                            return "[" + i + "]. " + entry.getValue() + " x " + entry.getKey().getName() + ": " + entry.getKey().getPrice() * entry.getValue() + "\n";
                         })
                         .collect(Collectors.joining()) +
                 "Total price: " + cart.getPrice();
