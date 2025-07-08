1f2f35b936e9d4aef7479fd9bc960bb50cb9f896
muffindud <ccatlabuga2002@gmail.com>
2025-07-08 16:43:05 +0300

    remove unused logger

--- src/main/java/com/github/muffindud/App.java
+++ src/main/java/com/github/muffindud/App.java
@@ -2,9 +2,7 @@ package com.github.muffindud;
 
 import com.github.muffindud.service.BaseService;
 import com.github.muffindud.service.impl.PizzaLand;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class App {
     public static final BaseService service = new PizzaLand();
 
