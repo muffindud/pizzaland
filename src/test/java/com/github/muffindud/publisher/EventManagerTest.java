00c3ba25d8ea2756876cffbaf510fe9bc007febf
muffindud <ccatlabuga2002@gmail.com>
2025-07-18 11:10:48 +0300

    add tests for EventManager and EventListener

--- src/test/java/com/github/muffindud/publisher/EventManagerTest.java
+++ src/test/java/com/github/muffindud/publisher/EventManagerTest.java
@@ -1,20 +1,52 @@
 package com.github.muffindud.publisher;
 
+import com.github.muffindud.enums.NotificationTopic;
+import com.github.muffindud.listener.EventListener;
 import org.testng.annotations.Test;
 
+import static org.mockito.Mockito.*;
+
 public class EventManagerTest {
-    @Test
-    public void testSubscribe() {
+    private static final NotificationTopic topic1 = mock(NotificationTopic.class);
+    private static final NotificationTopic topic2 = mock(NotificationTopic.class);
 
+    @Test
+    public void testUnsubscribeDoesntNotify() {
+        EventManager manager = mock(EventManager.class);
+        EventListener listener = mock(EventListener.class);
+
+        manager.subscribe(listener, EventManagerTest.topic1);
+        manager.unsubscribe(listener, EventManagerTest.topic1);
+        manager.notifySubscribers(EventManagerTest.topic1, null);
+
+        verify(listener, times(0)).update(EventManagerTest.topic1, null);
     }
 
     @Test
-    public void testUnsubscribe() {
+    public void testSubscribeNotifies() {
+        EventManager manager = new EventManager();
+        EventListener listener = mock(EventListener.class);
 
+        doNothing().when(listener).update(isA(NotificationTopic.class), isA(Object.class));
+
+        manager.subscribe(listener, EventManagerTest.topic1);
+        manager.notifySubscribers(EventManagerTest.topic1, null);
+
+        verify(listener, times(1)).update(EventManagerTest.topic1, null);
     }
 
     @Test
-    public void testNotifySubscribers() {
+    public void testMultipleSubscribeNotifies() {
+        EventManager manager = new EventManager();
+        EventListener listener = mock(EventListener.class);
 
+        manager.subscribe(listener, EventManagerTest.topic1);
+        manager.subscribe(listener, EventManagerTest.topic2);
+
+        manager.notifySubscribers(EventManagerTest.topic1, null);
+        manager.notifySubscribers(EventManagerTest.topic2, null);
+
+        verify(listener, times(1)).update(EventManagerTest.topic1, null);
+        verify(listener, times(1)).update(EventManagerTest.topic2, null);
     }
 }
